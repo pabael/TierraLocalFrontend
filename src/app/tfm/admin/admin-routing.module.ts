a6787677e76c9be700460a6b47ada8ee10e3f050
[Paloma Balmori Elosegui] <[pbalmorie@uoc.edu]>
2024-11-23 11:01:16 +0100

    dashboard with its own menu

--- src/app/tfm/admin/admin-routing.module.ts
+++ src/app/tfm/admin/admin-routing.module.ts
@@ -2,20 +2,27 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BrandFormPageComponent } from './pages/brand-form-page/brand-form-page.component';
 import { CreatePageComponent } from './pages/create-page/create-page.component';
+import { AdminPageComponent } from './pages/admin-page/admin-page.component';
 
 const routes: Routes = [
   {
-    path: 'newBrand',
-    component: BrandFormPageComponent
-  },
-  {
-    path: 'create',
-    component: CreatePageComponent
+    path: '',
+    component: AdminPageComponent,
+    children: [
+      {
+        path: 'newBrand',
+        component: BrandFormPageComponent
+      },
+      {
+        path: 'create',
+        component: CreatePageComponent
+      }
+    ]
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 export class AdminRoutingModule { }
